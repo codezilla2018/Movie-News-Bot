ef2177b414825219d0edd6bf26a5385f01774267
ManpowerTracking <asithaindrajithk9@gmail.com>
2018-05-21 19:19:01 +0530

    version 0.0.3

--- bot.js
+++ bot.js
@@ -9,30 +9,27 @@ var T = new Twit(config);//create new twit object with config module
 tweet();
 setInterval(tweet,1000*60*60*24);
 function tweet(){
-    var params=[{q: '@screenrant',count: 1},{q: '@CinemaBlend',count: 1},{q: '@IMDb',count: 1},{q: '@CBR',count: 1},{q: '@movieweb',count: 1},{q: '@comingsoonnet',count: 1},{q: '@darkhorizons',count: 1},{q: '@Collider',count: 1},{q: '@RottenTomatoes',count: 1}];
+    var params=[{q: '@screenrant',count: 1},{q: '@CinemaBlend',count: 1},{q: '@IMDb',count: 1},{q: '@CBR',count: 1},{q: '@movieweb',count: 1},{q: '@comingsoonnet',count: 1},{q: '@darkhorizons',count: 1},{q: '@RottenTomatoes',count: 1}];
     for(j=0;j<params.length;j++){
         T.get('search/tweets', params[j], gotData);//get data about 2 queries(q) we need
-    function gotData(err, data, response){
-        //console.log(data);  //and we write the data about query
-        if(!err){
+        function gotData(err, data, response){
             for(i=0;i<data.statuses.length;i++){
-            var tweet={
-                status: '#Movienews '+data.statuses[i].text
-            }
-            //console.log('#Movienews '+data.statuses[i].text);
-            T.post('statuses/update', tweet,tweeted);//This posts tweets mentioned in tweet param.
-            }
+                
+                var tweet={
+                    status: '#Movienews '+data.statuses[i].text
+                }
+                //console.log('#Movienews '+data.statuses[i].text);
+                T.post('statuses/update', tweet,tweeted);//This posts tweets mentioned in tweet param.
+                }
         }
-    }
-    function tweeted(err, data, response) {//this is a call back function work after tweet has posted.
-        if(err){
-            console.log(err);  
-        }
-        else{
-            console.log("Bot Start Tweeting!!!");
+        function tweeted(err, data, response) {//this is a call back function work after tweet has posted.
+            if(err){
+                console.log(err);  
+            }
+            else{
+                console.log("Bot Start Tweeting!!!");
+            }
         }
     }
 }
-    
-    }
     
